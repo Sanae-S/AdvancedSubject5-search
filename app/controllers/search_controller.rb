99d837fa55b154970e7f809e6451b385ac609174
vagrant <vagrant@localhost.localdomain>
2020-09-07 18:34:33 +0000

    [Finish]search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,33 +1,34 @@
 class SearchController < ApplicationController
+  before_action :authenticate_user!
 
   def search
-  	@model = params["search"]["model"]
+    @model = params["search"]["model"]
     @content = params["search"]["content"]
     @how = params["search"]["how"]
-    @datas = search_for(@how, @model, @content)
+    @datas = search_for(@model, @content, @how)
   end
 
   private
   def search_for(model, content, how)
-    if model == "user"
-      if how == "perfect" #完全一致
+    if model == 'user'
+      if how == 'perfect'
         User.where(name: content)
-      elsif how == "forward" #前方一致
-        User.where("name LIKE ?", "#{content}%")
-      elsif how == "backward" #後方一致
-        User.where("name LIKE ?", "%#{content}")
-      else #部分一致
-        User.where("name LIKE ?", "%#{content}%")
-      end
-    elsif model == "book"
-      if how == "perfect"
-        Book.where(title: content)
-      elsif how == "forward"
-        Book.where("title LIKE ?", "#{content}%")
-      elsif how == "backward"
-        Book.where("title LIKE ?", "%#{content}")
+      elsif how == 'forward'
+        User.where('name LIKE ?', content+'%')
+      elsif how == 'backward'
+        User.where('name LIKE ?', '%'+content)
       else
-        Book.where("title LIKE ?", "%#{content}%")
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == 'book'
+      if how == 'perfect'
+        Book.where(title: content)
+      elsif how == 'forward'
+        Book.where('title LIKE ?', content+'%')
+      elsif how == 'backward'
+        Book.where('title LIKE ?', '%'+content)
+      else
+        Book.where('title LIKE ?', '%'+content+'%')
       end
     end
   end
